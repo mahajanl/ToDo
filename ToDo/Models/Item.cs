bedf1923e5c1cf4d4d327d4cdd2ac367eca53a1c
mahajanl <lakshamima@gmail.com>
2017-10-27 10:22:16 -0400

    creating model Item

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,14 @@ namespace ToDo.Models
 {
     public class Item
     {
+        [Key]
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public DateTime ItemDueDate { get; set; }
+        public string ItemDetails { get; set; }
+        public bool ItemIsDone { get; set; }
+
+
+
     }
 }
