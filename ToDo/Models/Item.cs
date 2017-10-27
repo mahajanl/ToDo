97adf7ca69371fedc94dfd24c11a98d5aa968924
mahajanl <lakshamima@gmail.com>
2017-10-27 10:38:17 -0400

    finished up model list

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -15,7 +16,9 @@ namespace ToDo.Models
         public string ItemDetails { get; set; }
         public bool ItemIsDone { get; set; }
 
-
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
 
     }
 }
--- ToDo/Models/List.cs
+++ ToDo/Models/List.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,12 @@ namespace ToDo.Models
 {
     public class List
     {
+        [Key]
+        public int ListID { get; set; }
+        public string ListName { get; set; }
+
+        public virtual ICollection<Item> Item { get; set; }
+
+
     }
 }
